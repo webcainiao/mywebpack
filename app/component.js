5b8f510b1566660ed32e4e57ec14f12851623d78
webcainiao <2401800893@qq.com>
2016-12-22 15:46:55 +0800

    4

--- app/component.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = function(){
-	var element = document.createElement('h1');
-	element.innerHTML = 'Hello World nihaoya dbd d jjjjfdf';
-	return element;
-};
--- app/main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-require('./main.css')
-var component = require('./component.js');
-document.body.append(component());
